109b45b3c51b00cea94b49dae19db872086e6d63
simranjeetsi <simranjeetsi@gmail.com.com>
2024-01-15 02:56:26 -0500

    JSX, Fucntional Components

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-
-const parent = React.createElement("div",{id:"parent"}, 
-    React.createElement("div",{id:"child"}, 
-        [React.createElement("h1",{}, "Simranjeet Singh"), React.createElement("h2",{}, "I'm an h2 tag")]
-)
+// react element using core React
+const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Hello World"
 );
 
+// creating using  JSX
+
+const jsxHeading = <h1 className="head">Creating heading with JSX</h1>
 
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello from React!");
+// React Component
+// Class Based Component = OLD
+// Functional Component = NEW
+
+
+// React Fuctional component
+
+
+const TitleComponent = () => {
+    return <h3>Title Component</h3>
+}
+
+// const HeadingComponent = () =>{
+//     return <div><h1>Functional Component</h1><TitleComponent /></div>;
+// }
+
+
+const HeadingComponent = () =>(
+    <div>
+        <h1>Functional Component</h1>
+        <TitleComponent />
+    </div>
+);
+
+// const HeadingComponent = () =>(
+//     <div>
+//         <h1>Functional Component</h1>
+//         <TitleComponent ></TitleComponent>
+//     </div>
+// );
+
+// const HeadingComponent = () =>(
+//     <div>
+//         <h1>Functional Component</h1>
+//         {TitleComponent()}
+//     </div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//root.render(jsxHeading);
+
+root.render(<HeadingComponent />);
